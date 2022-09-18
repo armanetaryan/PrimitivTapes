4756b6a67acd2a1ffb69113dd136fe6837baca6b
Arman <armanetaryan07@gmail.com>
2022-09-18 11:55:57 +0400

    test

---
+++ src/Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println("c"+ c);
+
+
+    }
+}
