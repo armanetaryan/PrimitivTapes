18ac8e44b46560ff1b74b95d47a53be89f200bc2
Arman <armanetaryan07@gmail.com>
2022-09-24 14:14:43 +0400

    test

--- src/Operators.java
+++ src/Operators.java
@@ -1,87 +1,73 @@
 public class Operators {
     public static void main(String[] args) {
-
-        //max of numbers
-
-        int a  = 4, b = 3, c = 6, d = 9;
-        int max = a;
-
-        if (b > max)
-            max = b;
-        if (c > max)
-            max = c;
-        if (d > max)
-            max = d;
-
-        System.out.println("max is: " + max);
-
-
-        //Triangle
-
-        int a1 = 5;
-        int b1 = 6;
-        int c1 = 2;
-        if (a1 + b1 > c1 && a1 + c1 > b1 && c1 + b1 > a1)
-            System.out.println("with" + a1 + ", " + b1 + ", " + c1 + "is possible to make a triangle");
-        else
-            System.out.println("with" + a1 + ", " + b1 + ", " + c1 + "is not possible to make a triangle");
-
-
-        // Checking is triangle raight triangle or not
-
-        int slingshot = a1;
-        int page1 = b1;
-        int page2 = c1;
-        if (b1 > slingshot)
-            slingshot = b1;
-        page1 = a1;
-        if (c1 > slingshot)
-            slingshot = c1;
-        page2 = b1;
-        if (page1 * page1 + page2 * page2 == slingshot * slingshot)
-            System.out.println("The triangle is a right triangle");
-        else
-            System.out.println("The triangle is not a right triangle");
-
-
-
-        //week days
-
-        int date = 6;
-        switch (date){
-            case 1:
-                System.out.println("Monday " );
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-            case 7:
-                System.out.println("Weekend " + "\uD83E\uDD73" + "\uD83E\uDD73" + "\uD83E\uDD73"); // run to see
-                break;
-            default:
-                System.out.println("incorrect day number");
+        // Find even numbers between 0 and 100
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
         }
+        System.out.println();
+        System.out.println("*+*+*+*+*");
 
+        // Find odd numbers between 0 and 100
+        int i = 0;
+        while (i < 100) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+        System.out.println();
+        System.out.println("*+*+*+*+*");
 
+        // Find all numbers between 0 and 100 that are divisible by three
+        for (int j = 0; j < 100; j++) {
+            if (j % 3 == 0) {
+                System.out.print(j + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("*+*+*+*+*");
 
+        // Find the factorial of all numbers between 1 and 100
+        long f = 1L;
+        int i1 = 1;
+        while (i1 < 100) {
+            f *= i1;
+            System.out.println("Factorial of " + i1 + " is " + f);
+            i1++;
+        }
+        System.out.println();
+        System.out.println("*+*+*+*+*");
 
+        // Find the sum of all numbers between 0 and 100
+        int sum = 0;
+        for (int a = 0; a < 100; a++) {
+            sum += a;
+        }
+        System.out.println(sum);
+        System.out.println("*+*+*+*+*");
 
+       //figures
 
-
-
-
-
-
+        int x;
+        for (x = 0; x < 40; x++) {
+            int z;
+            for (z = x; z < 15 ; z++)
+                System.out.print("&^");
+            System.out.println();
+        }
+        //for if while
+        int in1 = 1;
+        while (in1 < 11) {
+            System.out.println("in1 is " + in1);
+            System.out.println("*+*+*+*+*");
+            for (int j = 1; j < 11; j++) {
+                System.out.println("j is " + j);
+            }
+            in1++;
+            System.out.println("*+*+*+*+*");
+        }
 
 
 
